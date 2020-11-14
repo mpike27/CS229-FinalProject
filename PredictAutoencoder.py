0a429f3f16c6c11bfa42a261895c7230e168010e
Takao Yatagai <takaoyatagai@MacBook-Pro.station>
2020-11-14 18:18:49 +0200

    enable predict using model downloaded from kaggle

--- PredictAutoencoder.py
+++ PredictAutoencoder.py
@@ -3,16 +3,17 @@ import Config
 from keras.layers import LayerNormalization
 from keras.models import load_model
 import AutoencoderParsing as ap
+import numpy as np
 
 def main():
     model = load_model(Config.MODEL_PATH,custom_objects={'LayerNormalization': LayerNormalization})
     print("got model")
-    test = ap.get_single_test()
-    print("got test - shape: ", test.shape)
+    test_sequences = ap.get_single_test()
+    print("got test - shape: ", test_sequences.shape)
 
     # get the reconstruction cost of all the sequences
-    reconstructed_sequences = model.predict(sequences,batch_size=4)
-    sequences_reconstruction_cost = np.array([np.linalg.norm(np.subtract(sequences[i],reconstructed_sequences[i])) for i in range(0,sz)])
+    reconstructed_sequences = model.predict(test_sequences,batch_size=4)
+    sequences_reconstruction_cost = np.array([np.linalg.norm(np.subtract(test_sequences[i],reconstructed_sequences[i])) for i in range(len(test_sequences))])
     sa = (sequences_reconstruction_cost - np.min(sequences_reconstruction_cost)) / np.max(sequences_reconstruction_cost)
     sr = 1.0 - sa
 
